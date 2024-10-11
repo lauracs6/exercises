6bd73be5bae1898c25b70840f0c6cbd16b12a08b
Laura Cabrera Sagrera <laura.s461441@cesurformacion.com>
2024-10-11 10:17:32 +0200

    Update rebajas25o50.java

--- rebajas25o50.java
+++ rebajas25o50.java
@@ -1,21 +1,33 @@
-public class MainClass {
-  public static void main(String[] args) {
-        
-    int numPrendas = 4; // 
-    float importeInicial = 54.26f; // 
-    float importeDescontado;
-    float importeFinal;
-    int Descuento1 = 25;
-    int Descuento2 = 50;
+import java.util.Scanner;
 
-    if (numPrendas <= 3) {
-      importeDescontado = importeInicial * Descuento1 / 100.0f;
-    } else {
-      importeDescontado = importeInicial * Descuento2 / 100.0f;
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Numero de prendas:");
+    int numPrendas = input.nextInt();
+
+    
+    System.out.println("Importe sin descuento:");
+    float importeInicial = input.nextFloat();
+          
+      float importeDescontado;
+      float importeFinal;
+      int Descuento1 = 25;
+      int Descuento2 = 50;
+
+       
+      if (numPrendas <= 3) {
+        importeDescontado = importeInicial * Descuento1 / 100.0f;
+      } else {
+        importeDescontado = importeInicial * Descuento2 / 100.0f;
+      }
+  
+      importeFinal = importeInicial - importeDescontado;
+  
+      System.out.println(importeFinal + " es el total de la compra.");
+
+    
     }
-
-    importeFinal = importeInicial - importeDescontado;
-
-    System.out.println(importeFinal + " es el total de la compra.");
   }
-}
